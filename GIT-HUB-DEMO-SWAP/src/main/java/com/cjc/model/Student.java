a15d5b34fb319390e0538ccfb621569ec04de56a
user <user@DESKTOP-STMNV1M>
2021-02-07 19:39:14 +0530

    Added Model class

---
+++ GIT-HUB-DEMO-SWAP/src/main/java/com/cjc/model/Student.java
@@ -0,0 +1,16 @@
+package com.cjc.model;
+
+public class Student 
+{
+	private int rollno;
+	private String name;
+	private String address;
+	
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", name=" + name + ", address=" + address + "]";
+	}
+	
+	
+
+}
