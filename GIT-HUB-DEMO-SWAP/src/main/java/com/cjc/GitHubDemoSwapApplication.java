836e69af36812d54831a7c3679bdbb569a810a23
User <swapnilp.gceme@gmail.com>
2021-02-07 18:56:31 +0530

    FIRST COMMIT

---
+++ GIT-HUB-DEMO-SWAP/src/main/java/com/cjc/GitHubDemoSwapApplication.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubDemoSwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubDemoSwapApplication.class, args);
+	}
+
+}
